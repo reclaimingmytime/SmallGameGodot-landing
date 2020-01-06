04ee147144dae6656457fcd7360b033416b951d8
reclaimingmytime <35609464+reclaimingmytime@users.noreply.github.com>
2020-01-06 13:33:14 +0100

    remove xhtml support from nl2br

--- js/main.js
+++ js/main.js
@@ -1,6 +1,5 @@
-function nl2br(str, is_xhtml) {
-    var breakTag = (is_xhtml || typeof is_xhtml === 'undefined') ? '<br />' : '<br>';
-    return (str + '').replace(/([^>\r\n]?)(\r\n|\n\r|\r|\n)/g, '$1' + breakTag + '$2');
+function nl2br(str) {
+    return (str + '').replace(/([^>\r\n]?)(\r\n|\n\r|\r|\n)/g, '$1<br>$2');
   }
   
   function escapeHtml(text) {
