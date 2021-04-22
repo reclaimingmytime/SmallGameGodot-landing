127aba8547c8a49621ebe3f70d142c020a162c9f
reclaimingmytime <35609464+reclaimingmytime@users.noreply.github.com>
2021-04-22 23:06:47 +0200

    open changelog in url

--- js/main.js
+++ js/main.js
@@ -18,36 +18,53 @@ function isHidden(el) {
   return (el.offsetParent === null);
 }
 
+function addChangelog() {
+  var changelogURL = "https://raw.githubusercontent.com/MisterL2/SmallGameGodot/master/Releases/Changelog.txt";
+
+  fetch(changelogURL)
+  .then(function (response) {
+    if(response.status < 200 || response.status >= 400) {
+      throw new Error("Unsuccessful request.");
+    }
+    return response.text();
+  })
+  .then(data => {
+    document.querySelector("#changelogCard").classList.add("border-info");
+    document.querySelector("#changelogContent").innerHTML = nl2br(sanitizeHTML(data));
+  }).catch(error => {
+    document.querySelector("#changelogCard").classList.add("border-danger");
+    document.querySelector("#changelogContent").innerHTML = '<h5 class="card-title">Error loading changelog.</h5> <a class="btn btn-primary" target="_blank" rel="nofollow noopener noreferrer" href="' + changelogURL + '">View in browser</a>';
+    });
+}
+function scrollToChangelog() {
+  window.location.href = "#changelogview";
+}
+
 /**
- * Retrieves Changelog
+ * Retrieves and scrolls to changelog
  */
 retrievedChangelog = false;
+if(window.location.hash == "#changelogview") {
+  addChangelog();
+  retrievedChangelog = true;
+
+  $("#changelogNotice").collapse('show');
+  $('#changelogNotice').on('shown.bs.collapse', function (e) {
+    scrollToChangelog();
+  });
+}
+
 document.querySelector("#changelog").addEventListener("click", function() {
   if (!retrievedChangelog) {
-    var changelogURL = "https://raw.githubusercontent.com/MisterL2/SmallGameGodot/master/Releases/Changelog.txt";
-
-    fetch(changelogURL)
-    .then(function (response) {
-      if(response.status < 200 || response.status >= 400) {
-        throw new Error("Unsuccessful request.");
-      }
-      return response.text();
-    })
-    .then(data => {
-      document.querySelector("#changelogCard").classList.add("border-info");
-      document.querySelector("#changelogContent").innerHTML = nl2br(sanitizeHTML(data));
-    }).catch(error => {
-      document.querySelector("#changelogCard").classList.add("border-danger");
-      document.querySelector("#changelogContent").innerHTML = '<h5 class="card-title">Error loading changelog.</h5> <a class="btn btn-primary" target="_blank" rel="nofollow noopener noreferrer" href="' + changelogURL + '">View in browser</a>';
-      });
-
+    addChangelog();
     retrievedChangelog = true;
   }
 
   if (isHidden(document.querySelector('#changelogContent'))) {
     $('#changelogNotice').on('shown.bs.collapse', function (e) {
-      window.location.href = "#changelogview";
+      if(window.location.hash != "#changelogview") scrollToChangelog();
     });
+  } else {
+    history.replaceState('', '', ' '); //remove hash from url
   }
-
 });
